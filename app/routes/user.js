b06b84f92ab62e6f645a89c9e871e24a9e1d2a2b
HYUNGWU PAE <monad98@naver.com>
2017-05-11 23:22:24 -0700

    bubble chart

--- app/routes/user.js
+++ app/routes/user.js
@@ -5,7 +5,6 @@ const controller = require('../controller/user.controller');
 
 router.get('/', controller.index);
 router.get('/me', controller.me);
-router.get('/:id/chart-test', controller.showChart);
 router.get('/:id', controller.show);
 
 router.put('/profile-image', controller.saveProfileImageUrl);
